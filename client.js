d6e258a6412178d26494cebf3573521704ac46ab
Mikhail Serebriakov <mishacyb@gmail.com>
2020-05-06 20:06:29 +0000

    Implemented sending a name to the server

--- client.js
+++ client.js
@@ -8,6 +8,11 @@ const connect = function() {
 
   conn.setEncoding('utf8');
 
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: MS");
+  });
+
   conn.on('data', (message) => {
     console.log('Message from server:', message);
   });
