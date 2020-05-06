99d81a3b0363027d3fe15f9df70498eca905f039
Mikhail Serebriakov <mishacyb@gmail.com>
2020-05-06 19:55:23 +0000

    Added play.js

---
+++ play.js
@@ -0,0 +1,21 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: '50.64.116.162',
+    port: 50541
+  });
+
+  conn.setEncoding('utf8');
+
+  conn.on('data', (message) => {
+    console.log('Message from server:', message);
+  });
+
+  return conn;
+}
+
+
+
+console.log('Connecting ...');
+connect();
